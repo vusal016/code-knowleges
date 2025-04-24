92d96012584845e7e1b29fd39cc8b449ce75cdd0
vusal016 <mvusal316@gmail.com>
2025-04-24 20:03:59 +0400

    Added

---
+++ Variable tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+int x = 13;
+int n = 13;
+
+if (x % 3 == 0)
+{
+
+    Console.WriteLine("salamlar");
+
+}
+else if (x % 3 == 1)
+{
+    Console.WriteLine("gorusenedek");
+}
+
+else
+{
+    Console.WriteLine("sag ol");
+}
+
+
+if (x > 10 && x < 15)
+{
+    Console.WriteLine("hello world");|
+}
+else
+{
+    Console.WriteLine("goodbye world");
+}
+
+if (n % 3 = 0 && n % 7 = 0) ;
+{
+    Console.WriteLine("everything will be okay");
+}
+else
+{
+    Console.WriteLine("every moment is special")
+}
